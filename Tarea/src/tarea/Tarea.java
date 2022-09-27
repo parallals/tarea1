e67811f206eaee0a1fae6954f122d875b5b5a7e2
parallals <frjelvez2021@udec.cl>
2022-09-27 16:34:56 -0300

    Tarea

--- Tarea/src/tarea/Tarea.java
+++ Tarea/src/tarea/Tarea.java
@@ -3,7 +3,33 @@ package tarea;
 public class Tarea {
 
     public static void main(String[] args) {
-
+        Cliente a = new Cliente("pocoyó","21.250.675-3");
+        a.toStringCliente();
     }
+    
+}
+
+
+class Cliente{
+    private String nombre;
+    private String rut;
+    public String getNombre(){
+        return nombre;
+    }
+    public String getRut(){
+        return rut;
+    }
+    public void toStringCliente(){
+        System.out.println(this.nombre + ", Rut: " + this.rut);
+    }
+    public Cliente(String nombre, String rut){
+        this.nombre = nombre;
+        this.rut = rut;
+    }
+}
+
+class OrdenCompra{
+    private Date fecha;
+    private String estado;
     
 }
