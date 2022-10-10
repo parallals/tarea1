a65d513be7c5e978dec8d6a4caf81e36a8d1818d
parallals <frjelvez2021@udec.cl>
2022-10-09 23:28:11 -0300

    main edit

--- Tarea/src/tarea/Tarea.java
+++ Tarea/src/tarea/Tarea.java
@@ -4,31 +4,66 @@ import java.util.ArrayList;
 
 public class Tarea {
     public static void main(String[] args) {
-        Date date1 = new Date();
-        Date date2 = new Date();
+
         Articulo art1 = new Articulo(1,"jugo","de piña",3000);
         Articulo art2 = new Articulo(2,"papas","kryzpo",2000);
         Articulo art3 = new Articulo(3,"Tarjeta de video Nvidia Zotac Gaming GForce GTX 16 Series","and knuckles",30000);
         Articulo art4 = new Articulo(4,"Un Frugele","el rojo",100);
         Articulo art5 = new Articulo(3,"Completo","Papapleto",1000);
+        
         DetalleOrden det1 = new DetalleOrden(3, art1);
         DetalleOrden det2 = new DetalleOrden(5, art2);
         DetalleOrden det3 = new DetalleOrden(1, art3);
         DetalleOrden det4 = new DetalleOrden(4, art4);
         DetalleOrden det5 = new DetalleOrden(2, art5);
+        
+        Direccion adress1 = new Direccion(" no se aaaaah 1");
+        Direccion adress2 = new Direccion(" no se aaaaah 2");
+        
         Cliente diego1 = new Cliente("Diego bueno que escribe el Detalle","21222222-2");
-        Cliente diego2 = new Cliente("Diego malo que insiste en no usar ArrayList","21222222-3");
-        Efectivo pagoEf1 = new Efectivo(100, date2);
-        Efectivo pagoEf2 = new Efectivo(50000, date2);
-        Transferencia pagoTr = new Transferencia("Banco Estado", "12919292",2000, date2);
-        Tarjeta pagoTa = new Tarjeta("Banco Fallabella", "i1238", 20500, date2);
-        DetalleOrden[] lista1 = {det1,det2,det3};
+        Cliente diego2 = new Cliente("Diego malo que insiste en no usar ArrayList (Esto es FALSO)","21222222-3");
+        diego1.setDireccion(adress1);
+        diego2.setDireccion(adress2);
+        
+        Boleta bol = new Boleta();
+        bol.setCliente(diego1);
+        Factura fac = new Factura();
+        fac.setCliente(diego2);
+        
+        Efectivo pagoEf1 = new Efectivo(10000); //Pagos
+        Efectivo pagoEf2 = new Efectivo(50000);
+        Efectivo pagoEf3 = new Efectivo(10000); 
+        Efectivo pagoEf4 = new Efectivo(50000);
+        Transferencia pagoTr = new Transferencia("Banco Estado", "12919292",2000);
+        Tarjeta pagoTa = new Tarjeta("Banco Fallabella", "i1238", 20500);
+        
+        DetalleOrden[] lista1 = new DetalleOrden[3];
+        lista1[0] = det1;
+        lista1[1] = det2;
+        lista1[2] = det3;
         DetalleOrden[] lista2 = {det4,det5};
-        DetalleOrden[] lista3 = {det1,det2,det3,det4,det5};        
-        OrdenCompra oc1 = new OrdenCompra(date1, "Pendiente", lista1, diego1, pagoTr);
-        OrdenCompra oc2 = new OrdenCompra(date1, "Pendiente", lista2, diego2, pagoEf1);
-        OrdenCompra oc3 = new OrdenCompra(date2, "Pendiente", lista3, diego2, pagoEf2);        
-        System.out.println(det1.toString());
+        DetalleOrden[] lista3 = {det1,det2,det3,det4,det5};
+        ArrayList<Pago> pago1 = new ArrayList();
+        pago1.add(pagoEf1);
+        pago1.add(pagoEf2);
+        ArrayList<Pago> pago2 = new ArrayList();
+        pago2.add(pagoTa);
+        pago2.add(pagoTr);
+        ArrayList<Pago> pago3 = new ArrayList();
+        pago3.add(pagoEf3);
+        pago3.add(pagoEf4);
+        OrdenCompra oc1 = new OrdenCompra(bol,lista1,diego1,pago1); //Boleta, pago 60000, diego1, 
+        OrdenCompra oc2 = new OrdenCompra(fac,lista2,diego2,pago2);
+        OrdenCompra oc3 = new OrdenCompra(bol,lista2,diego1,pago3);
+        pago1.get(0).setOrdenCompra(oc1);
+        pago1.get(1).setOrdenCompra(oc1);
+        pago3.get(0).setOrdenCompra(oc2);
+        pago3.get(1).setOrdenCompra(oc2);
+        oc1.verificarEstado();
+        System.out.println(oc1.getEstado());
+        oc2.verificarEstado();
+        System.out.println(oc2.getEstado());
+        System.out.println(oc2.toString());
     }
 }
 
@@ -38,18 +73,33 @@ class OrdenCompra{
     private String estado;
     private DetalleOrden[] detalleorden;
     private Cliente cliente;
-    private Pago pago;
+    private ArrayList<Pago> pago;
     //Metodos
-    public OrdenCompra(Date fecha, String estado, DetalleOrden[] detalleorden, Cliente cliente, Pago pago){
-        this.fecha = fecha;
-        this.estado = estado;
-        for (int j = 0; j > detalleorden.length ; j++){
-        this.detalleorden[j] = detalleorden[j];
-        }
+    public OrdenCompra(DocTributario doctributario, DetalleOrden[] detalleorden, Cliente cliente, ArrayList<Pago> pago){
+        this.fecha = doctributario.getFecha();
+        this.estado = "Pendiente";
+        this.detalleorden = detalleorden;
         this.cliente = cliente;
         this.pago = pago;
     }
-    public float  calcPrecioSinIVA(){
+    public void verificarEstado(){
+        float aux = 0; //A pagar
+        for(int i=0 ; i<detalleorden.length ; i++){
+            aux = aux + detalleorden[i].calcPrecio();
+        }
+        Pago aux1;
+        float monto = 0; //Pago del cliente
+        for(int i = 0; i<pago.size();++i){
+            aux1 = pago.get(i);
+            monto = monto + aux1.getMonto();
+        }
+        if(aux <= monto){
+            estado = "Pagado";
+        }else{
+            estado = "Pendiente";
+        }
+    }
+    public float calcPrecioSinIVA(){
         float aux = 0;
         for(int i=0 ; i<detalleorden.length ; i++){
             aux = aux + detalleorden[i].calcPrecioSinIVA();
@@ -78,6 +128,12 @@ class OrdenCompra{
         return aux;
     }
     //Getters, Setters y toString
+    public DetalleOrden[] getDetalleOrden(){
+        return detalleorden;
+    }
+    public ArrayList<Pago> getPago(){
+        return pago;
+    }
     public Date getFecha(){
         return fecha;
     }
@@ -106,6 +162,13 @@ class Cliente{
     public Cliente(String nombre, String rut){
         this.nombre = nombre;
         this.rut = rut;
+        this.direccion = null;
+    }
+    public void setDireccion(Direccion direccion){
+        this.direccion = direccion;
+    }
+    public String getDireccion(){
+        return direccion.getDireccion();
     }
     //Getters, Setters y toString
     public String getNombre(){
@@ -148,20 +211,20 @@ class Direccion{
 
 abstract class DocTributario{
     //Propiedades
-    protected String numero;
-    protected String rut;             // En clases abstract usaremos protected en vez de private, puesto que simplifica mas las cosas y no es necesario usar getters.
-    protected Date fecha;
-    protected Cliente cliente;
+    private String direccion;
+    private String rut;
+    private Date fecha;
     //Metodos
-    public DocTributario(String numero, String rut, Date fecha, Cliente cliente){
-        this.numero = numero;
-        this.rut = rut;
+    public DocTributario(Date fecha){
         this.fecha = fecha;
-        this.cliente = cliente;
+    }
+    public void setCliente(Cliente cliente){
+        this.direccion = cliente.getDireccion();
+        this.rut = cliente.getRut();
     }
     //Getters, Setters
-    public String getNumero(){
-        return numero;
+    public String getDireccion(){
+        return direccion;
     }
     public String getRut(){
         return rut;
@@ -169,8 +232,8 @@ abstract class DocTributario{
     public Date getFecha(){
         return fecha;
     }
-    public void setNumero(String numero){
-        this.numero = numero;
+    public void setDireccion(String direccion){
+        this.direccion = direccion;
     }
     public void setRut(String rut){
         this.rut = rut;
@@ -178,31 +241,27 @@ abstract class DocTributario{
     public void setFecha(Date fecha){
         this.fecha = fecha;
     }
-    @Override
-    public String toString(){
-        return "Esta clase guarda:  -Numero:"+numero+" -Rut:"+rut+" -Fecha:"+fecha+" -Cliente:"+cliente+"  de la clase DocTributario";
-    }    
 }
 
 class Boleta extends DocTributario{
-    public Boleta(String numero, String rut, Date Fecha, Cliente cliente){
-        super(numero, rut, Fecha, cliente);      
+    public Boleta(){
+        super(new Date());      
     }
     //toString
     @Override
     public String toString(){
-        return "Esta clase guarda:  -Numero:"+numero+" -RUT:"+rut+" -Fecha: "+fecha+"  de la clase Boleta";
+        return "Esta clase guarda:  -Direccion:"+getDireccion()+" -RUT:"+getRut()+" -Fecha: "+getFecha()+"  de la clase Boleta";
     }
 }
 
 class Factura extends DocTributario{
-    public Factura(String numero, String rut, Date Fecha, Cliente cliente){
-        super(numero, rut, Fecha, cliente); 
+    public Factura(){
+        super(new Date());      
     }
     //toString
     @Override
     public String toString(){
-        return "Esta clase guarda:  -Numero:"+numero+" -RUT:"+rut+" -Fecha: "+fecha+"  de la clase Factura";
+        return "Esta clase guarda:  -direccion:"+getDireccion()+" -RUT:"+getRut()+" -Fecha: "+getFecha()+"  de la clase Factura";
     }
 }
 
@@ -287,8 +346,8 @@ class Articulo{
 //______________________________Informacion del Pago__________________________________//
 abstract class Pago{
     //Propiedades
-    protected float monto;             // En clases abstract usaremos protected en vez de private, puesto que simplifica mas las cosas y no es necesario usar getters.
-    protected Date fecha;
+    private  float monto;
+    private  Date fecha;
     private OrdenCompra ordencompra;
     //Metodos
     public Pago(float monto, Date fecha){
@@ -318,30 +377,45 @@ abstract class Pago{
 
 class Efectivo extends Pago{
     //Metodos
-    public void calcDevolucion(){
-        float aux1;
-        OrdenCompra aux2 = getOrdenCompra();
-        aux1 = monto - aux2.calcPrecio();
+    public float calcDevolucion(){
+        DetalleOrden[] detalleorden = new DetalleOrden[getOrdenCompra().getDetalleOrden().length];
+        detalleorden = getOrdenCompra().getDetalleOrden();
+        ArrayList<Pago> pago = getOrdenCompra().getPago();
+        float aux = 0; //A pagar
+        for(int i=0 ; i<detalleorden.length ; i++){
+            aux = aux + detalleorden[i].calcPrecio();
+        }
+        Pago aux1;
+        float monto = 0; //Pago del cliente
+        for(int i = 0; i<pago.size();++i){
+            aux1 = pago.get(i);
+            monto = monto + aux1.getMonto();
+        }
+        if(aux <= monto){
+            return monto-aux;
+        }else{
+            return 0f;
+        }
     }
-    public Efectivo(float monto, Date fecha){
-        super(monto, fecha);
+    public Efectivo(float monto){
+        super(monto, new Date());
     }
     //toString
     @Override
     public String toString(){
-        return "Esta clase guarda:  -Monto:"+monto+" -Fecha:"+fecha+"  de la clase Efectivo";
+        return "Esta clase guarda:  -Monto:"+getMonto()+" -Fecha:"+getFecha()+"  de la clase Efectivo";
     }
 }
 
-class Transferencia extends Pago{
-    //Propiedades
+class Transferencia extends Pago{    //Propiedades
     private String banco;
     private String numCuenta;
     //Metodos
-    public Transferencia(String banco, String numCuenta, float monto, Date fecha){
-        super(monto, fecha);
+    public Transferencia(String banco, String numCuenta, float monto){
+        super(monto, new Date());
         this.banco = banco;
         this.numCuenta = numCuenta;
+        
     }
     //Getters, Setters y toString
      public String getBanco(){
@@ -358,7 +432,7 @@ class Transferencia extends Pago{
     }
     @Override
     public String toString(){
-        return "Esta clase guarda:  -Monto:"+monto+" -Fecha:"+fecha+" -Banco:"+banco+" -NumeroCuenta:"+numCuenta+"  de la clase Transferencia";
+        return "Esta clase guarda:  -Monto:"+getMonto()+" -Fecha:"+getFecha()+" -Banco:"+banco+" -NumeroCuenta:"+numCuenta+"  de la clase Transferencia";
     }
 }
 
@@ -367,8 +441,8 @@ class Tarjeta extends Pago{
     private String tipo;
     private String numtransaccion;
     //Metodos
-    public Tarjeta(String tipo, String numtransaccion, float monto, Date fecha){
-        super(monto, fecha);
+    public Tarjeta(String tipo, String numtransaccion, float monto){
+        super(monto, new Date());
         this.tipo = tipo;
         this.numtransaccion = numtransaccion;
     }
@@ -387,6 +461,6 @@ class Tarjeta extends Pago{
     }
     @Override
     public String toString(){
-        return "Esta clase guarda:  -Monto:"+monto+" -Fecha:"+fecha+" -Tipo:"+tipo+" -NumeroTransaccion:"+numtransaccion+"  de la clase Tarjeta";
+        return "Esta clase guarda:  -Monto:"+getMonto()+" -Fecha:"+getFecha()+" -Tipo:"+tipo+" -NumeroTransaccion:"+numtransaccion+"  de la clase Tarjeta";
     }
 }
